d2665f640bdff5393a13a90257f1854adbfc390e
lingul <57398223+lingul@users.noreply.github.com>
2020-10-16 11:42:01 +0200

    Update events.js

--- js/events.js
+++ js/events.js
@@ -27,11 +27,11 @@ $('#endTime').on("click", function () {
 	$('#availablilities-list').empty();
 });
 
-// use add button instead? 
+// use add button instead?
 $('.btn.addUser').on('click', function (event) {
 	var inviteEmail = $('#form-invitedUser').val();
 	var mailformat = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-	if (inviteEmail !== "" && inviteEmail.match(mailformat)) 
+	if (inviteEmail !== "" && inviteEmail.match(mailformat))
 	{
 		searchUsersList(inviteEmail).then(function() {
 
@@ -61,13 +61,13 @@ $('.btn.addUser').on('click', function (event) {
 			newLi.removeAttribute('id');
 
 			document.getElementById('form-invitedUser').value = "";
-			
+
 			userArray.push(inviteEmail);
 
 			$('.closeSpan').on("click", function (event) {
 				userArray.splice(userArray.indexOf(event), 1);
 				verifiedIDs.splice(verifiedIDs.indexOf(event), 1);
-				
+
 				$(this).parent().remove();
 			});
 
@@ -83,144 +83,6 @@ window.onclick = function (event) {
 	}
 }
 
-/*
-function createEvent() {
-	var i;
-
-	//array to hold the checkboxes for repititions
-	var repArray = new Array(0);
-
-	var eventDate = $('#datepicker').val();
-
-	var eventStart = $('#startTime').val();
-
-	var eventEnd = $('#endTime').val();
-
-	var eventTitle = $('#form-title').val();
-
-	var eventLocation = $('#form-location').val();
-
-	var eventTimezone = $('#form-timezone').val();
-
-	var eventDescription = $('#form-desc').val();
-
-	var eventRepetition = document.querySelectorAll('.rep-box');
-
-	for (i = 0; i < eventRepetition.length; i++) {
-		if (eventRepetition[i].checked) {
-			repArray.push(eventRepetition[i].value);
-		}
-	}
-
-	var repFrequency = $('#rep').val();
-
-	var eventReminders = $('#form-reminders').val();
-
-	var privacyValue = $('#privacy').val();
-
-	var eventID = generateToken();
-
-	writeUserData(eventID, eventDate, eventStart, eventEnd, eventTitle, eventLocation,
-		eventTimezone, eventDescription, repArray, repFrequency, eventReminders,
-		privacyValue, userArray);
-
-};
-*/
-//take form data and create a JSON object of all the data and
-//upload to firebase database /eventss
-/*
-function writeUserData(eI, eDay, eS, eE, eT, eL, eTz, eDesc, rA, rF, eR, pV, iA) {
-
-	// if these values are not entered in the form, assign them as null or else they aren't added to FireBase database
-	if(rA.length == 0)
-	{
-		rA = "none";
-	}
-	if(iA.length == 0)
-	{
-		iA = "none";
-	}
-	if(eR == "")
-	{
-		eR = "none";
-	}
-	if(eTz == undefined)
-	{
-		eTz = "none";
-	}
-	if(rF == undefined)
-	{
-		rF = "none";
-	}
-
-	//set() overwrites data at the specified location (here events/eventID)
-	firebase.auth().onAuthStateChanged(function (user) {
-
-		if (user) {
-			//user is signed in
-			firebase.database().ref('events/' + eI).set(
-				{
-					eventOwner: firebase.auth().currentUser.displayName,
-					eventOwnerEmail: firebase.auth().currentUser.email,
-					eventID: eI,
-					eventDate: eDay,
-					eventStartTime: eS,
-					eventEndTime: eE,
-					eventTitle: eT,
-					eventLocation: eL,
-					eventTimezone: eTz,
-					eventDescription: eDesc,
-					repetitionDaysArray: rA,
-					repetitionFrequency: rF,
-					eventReminders: eR,
-					privacySetting: pV,
-					invitedUsers: iA
-
-				}).then(function () {
-
-					addInvitedUsers(eI);
-
-					//add the newly created event to the user's list of participating events
-					var userID = firebase.auth().currentUser.uid;
-					var userEmail = firebase.auth().currentUser.email;
-
-					firebase.database().ref('users/' + userID).once('value').then(function (snapshot) {
-
-						var eventArray = snapshot.val().events;
-
-						if (eventArray[0] === "0") {
-							eventArray.shift();
-							eventArray.push(eI);
-							updateEventsArray(eventArray, userEmail, userID);
-						}
-						else {
-							eventArray.push(eI);
-							updateEventsArray(eventArray, userEmail, userID);
-						}
-
-						alert("Event created successfully!");
-
-						window.location.href = "index.php"
-
-					});
-
-				}).catch(function (error) {
-
-					var errorMessage = error.message;
-
-					alert("ERROR: " + errorMessage);
-
-				});
-		}
-		else {
-			//user is not signed in
-			alert("ERROR: Must be logged in to setup a new event");
-		}
-
-	});
-
-}
-*/
 function rand() {
     return Math.random().toString(36).slice(2, 10); // remove `0.`
 };
@@ -232,79 +94,6 @@ function generateToken() {
 
 };
 
-/*
-function updateEventsArray(eventsArr, userEmail, userID) {
-
-	firebase.auth().onAuthStateChanged(function (user) {
-
-		if (user) {
-			//user is signed in
-			firebase.database().ref('users/' + userID).set(
-				{
-
-					userEmail: userEmail,
-					events: eventsArr
-
-				});
-
-		}
-		else {
-			// no user signed in
-			alert("Must be logged in to do that");
-		}
-
-	});
-
-};
-
-function addInvitedUsers(eventID) {
-
-	var invitedUsers;
-
-    firebase.database().ref('events/' + eventID).once("value").then(function(snapshot) {
-
-        invitedUsers = snapshot.val().invitedUsers.slice();
-
-        if(invitedUsers !== "none")
-        {
-
-	        firebase.database().ref('users/').once("value").then(function(snapshot) {
-
-	            snapshot.forEach(function(childSnapshot) {
-
-	                var userEmail = childSnapshot.val().userEmail;
-	                if(invitedUsers.indexOf(userEmail) >= 0)
-	                {
-	                    //the userEmail is in the list of invited users, add the event to their collection
-
-	                    var userID = childSnapshot.key;
-	                    //get a copy of the events array that holds events belonging to user
-	                    var eventArray = childSnapshot.val().events.slice();
-
-                        // add this event to the collection of events for this user
-						if (eventArray[0] === "0") 
-						{
-							eventArray.shift();
-							eventArray.push(eventID);
-							updateEventsArray(eventArray, userEmail, userID);
-						}
-						else 
-						{
-							eventArray.push(eventID);
-							updateEventsArray(eventArray, userEmail, userID);
-						}
-
-	                }
-
-	            });
-
-	        });
-        	
-        }
-    });
-
-};
-*/
 function searchUsersList(inviteEmail) {
 	return new Promise(function(resolve, reject) {
 		const find = verifiedIDs.find(element => element === inviteEmail);
@@ -329,9 +118,9 @@ function checkOverlapEvents(eventStart, eventEnd, eventDate, userID) {
 					var newStartTime = eventStart;
 					var eventStartTime = res.val().eventStartTime;
 					var newEndTime = eventEnd;
-					var eventEndTime = res.val().eventEndTime;    								
+					var eventEndTime = res.val().eventEndTime;
 					if(eventDate === res.val().eventDate) {
-						if(newStartTime >= eventStartTime && newStartTime < eventEndTime || 
+						if(newStartTime >= eventStartTime && newStartTime < eventEndTime ||
 							eventStartTime >= newStartTime && eventStartTime < newEndTime) {
 							//overlap found
 							return reject();
@@ -340,142 +129,12 @@ function checkOverlapEvents(eventStart, eventEnd, eventDate, userID) {
 					else if(index === array.length - 1) //at last index of array (no overlaps found)
 					{
 						return resolve();
-					}    								
+					}
 				});
 			});
 		});
 	});
 };
-/*
-function checkFields(eventDate, start, end) {
-    return new Promise(function(resolve, reject) {
-
-        var inputtedYear = eventDate.substr(0, 4);
-        var inputtedMonth = eventDate.substr(5, 2);
-        var inputtedDay = eventDate.substr(8, 2);
-
-        var today = new Date();
-        var dd = today.getDate();
-        var mm = today.getMonth()+1;
-        var yyyy = today.getFullYear();
-
-        if(dd < 10) {
-            dd = '0' + dd;
-        }
-
-        if(mm < 10)
-        {
-            mm = '0' + mm;
-        }
-
-        if (inputtedYear < yyyy) {
-            return reject("Date can not be before current date");
-        }
-        else if(inputtedYear == yyyy)
-        {
-            if(inputtedMonth < mm)
-            {
-                return reject("Date can not be before current date");
-            }
-            else if(inputtedMonth == mm)
-            {
-                if(inputtedDay < dd) //date is before current day
-                {
-                    return reject("Date can not be before current date");
-                }
-                else if(inputtedDay == dd) //date is the same as current day
-                {
-                    var hh = today.getHours();
-
-                    if(hh < 10)
-                    {
-                        hh = '0' + hh;
-                    }
-
-                    var mm = today.getMinutes();
-
-                    if(mm < 10)
-                    {
-                        mm = '0' + mm;
-                    }
-
-                    var time = hh + ":" + mm;
-
-                    //if the event end time is less than current time
-                    if(start < time)
-                    {
-                        return reject("Event start time can not be before current time");
-                    }
-                    else if(end < time)
-                    {
-                        return reject("Event end time can not be before current time"); //if event time is after current time
-                    }
-                    else if(start == end)
-                    {
-                        return reject("Event starting and ending times can not be the same");
-                    }
-                    else
-                    {
-                        return resolve();
-                    }
-
-                }
-                else if(inputtedDay > dd) //date is after current day
-                {
-                    //if the event end time is less than current time
-                    if(start > end)
-                    {
-                        return reject("Event start time can not be greater than ending event time");
-                    }
-                    else if(start == end)
-                    {
-                        return reject("Event starting and ending times can not be the same");
-                    }
-                    else
-                    {
-                        return resolve();
-                    }
-                }
-            } 
-            else if(inputtedMonth > mm)
-            {
-                //if the event end time is less than current time
-                if(start > end)
-                {
-                    return reject("Event start time can not be greater than ending event time");
-                }
-                else if(start == end)
-                {
-                    return reject("Event starting and ending times can not be the same");
-                }
-                else
-                {
-                    return resolve();
-                }
-            }
-
-        }
-        else if(inputtedYear > yyyy)
-        {
-            //if the event end time is less than current time
-            if(start > end)
-            {
-                return reject("Event start time can not be greater than ending event time");
-            }
-            else if(start == end)
-            {
-                return reject("Event starting and ending times can not be the same");
-            }
-            else
-            {
-                return resolve();
-            }
-        }
-
-    });
-
-}; 
-*/
 
 function insertTime(allEvents, start_or_end, time) {
 	let timeInt = time.substring(0,2)+time.substring(3);
@@ -495,133 +154,6 @@ function insertTime(allEvents, start_or_end, time) {
 	return Promise.resolve();
 };
 
-// Matching schedules looks at each day and splits up events if that event overlaps midnight, it gets cutoff at midnight
-/*
-async function matchSchedules(day, minStartTime, duration) {
-	console.log("matching schudles");
-
-	// if verified users.size == 0, quit
-	if (verifiedIDs.length == 0) {
-		console.log("There are no verifiedIDs");
-		return;
-	}
-	// Get events for each user in verified users
-	let checkedEvents = new Set();
-	let allEvents = [];
-	await Promise.all(verifiedIDs.map(async function(userID) {
-		if ($('#startTime').val() != "" && $('#endTime').val() != "") {
-			await checkOverlapEvents($('#startTime').val(), $('#endTime').val(), day, userID).catch(function() {
-				alert("Your input event times overlaps with an attendee's schedule. Please choose one of the available times from the list.");
-				document.getElementById("startTime").value = "";
-				document.getElementById("endTime").value = "";
-			});
-		}
-		
-		await firebase.database().ref('users/'+userID+'/events/').once("value")
-		.then(async function(eventIDs) {
-			return await Promise.all(eventIDs.val().map(async function(eventID) {
-				if (!checkedEvents.has(eventID)) {
-					checkedEvents.add(eventID);
-					// insertion sort event start time as "start"
-					await firebase.database()
-					.ref('events/'+eventID+'/')
-					.once("value")
-					.then(async function(eventSnapshot) {
-						
-						// ###################################################################
-						// #  TODO: REMOVE commented-if-statement TO FILTER BY SELECTED DAY  #
-						// ###################################################################
-
-						if (1/*eventSnapshot.val().eventDate == day) {
-							let startTime = eventSnapshot.val().eventStartTime;
-							let endTime = eventSnapshot.val().eventEndTime;
-							await insertTime(allEvents, "start", startTime);
-							await insertTime(allEvents, "end", endTime);
-						}
-						
-					});
-				}
-
-			})); // after inner map
-		});
-
-	})); // after outer map
-	let testLength = allEvents.length;
-	if (testLength == 0 || testLength % 2 != 0) {
-		console.log("Error: Something went wrong gathering start/end times for all invitees.");
-		console.log("allEvents.length: "+ testLength);
-		return;
-	}
-	console.log("allEvents.length: "+ testLength);
-	let availableStart = -1;
-	if (allEvents[0].time != 0) {
-		availableStart = 0;
-	}
-	// go through timeline of all events
-	let availabilities = [];
-	let stack = [];
-	let allEventsLength = allEvents.length;
-	for (let i = 0; i < allEventsLength; ++i) {
-		if (allEvents[i].type == "start") {
-			stack.push(allEvents[i].time);
-			if (availableStart != -1) {
-				if (availableStart != allEvents[i].time) {
-					let startMins = await militaryToMinutes(availableStart);
-					let endMins = await militaryToMinutes(allEvents[i].time);
-					if ((endMins - startMins) == duration && availableStart >= minStartTime) {
-						availabilities.push(new AvailableTime(day, availableStart, allEvents[i].time));
-					}
-					else if ((endMins - startMins) > duration) {
-						// if startTime is before the minimum start time, shift it up before splitting by duration
-						if (availableStart < minStartTime) {
-							startMins = Math.floor(minStartTime/100) * 60 + (minStartTime%100);
-						}
-						while ((endMins - startMins) >= duration) {
-							let militaryStart = await minutesToMilitary(startMins);
-							startMins += duration;
-							let militaryEnd = await minutesToMilitary(startMins);
-							availabilities.push(new AvailableTime(day, militaryStart, militaryEnd));
-						}
-					}
-				}
-				availableStart = -1;
-			}
-		}
-		else {
-			stack.pop();
-			if (stack.length == 0) {
-				availableStart = allEvents[i].time;
-			}
-		}
-	}
-	if (availableStart == -1) {
-		console.log("Midnight was not capped for the day. The last event was cut off at midnight");
-	} else if (stack.length == 0) {
-		let midnight = 2400;
-		if (availableStart != midnight) {
-			let startMins = await militaryToMinutes(availableStart);
-			let endMins = await militaryToMinutes(midnight);
-			if ((endMins - startMins) == duration && availableStart >= minStartTime) {
-				availabilities.push(new AvailableTime(day, availableStart, midnight));
-			} else if ((endMins - startMins) > duration) {
-				// if startTime is before the minimum start time, shift it up before splitting by duration
-				if (availableStart < minStartTime) {
-					startMins = Math.floor(minStartTime/100) * 60 + (minStartTime%100);
-				}
-				while ((endMins - startMins) >= duration) {
-					let militaryStart = await minutesToMilitary(startMins);
-					startMins += duration;
-					let militaryEnd = await minutesToMilitary(startMins);
-					availabilities.push(new AvailableTime(day, militaryStart, militaryEnd));
-				}
-			}
-		}
-	}
-	verifiedIDs = [];
-
-	displayAvailabilities(availabilities);
-};
-*/
 function insertTime(allEvents, start_or_end, time) {
 	let timeInt = time.substring(0,2)+time.substring(3);
 	let t = new TimeDivider(start_or_end, parseInt(timeInt));
@@ -638,17 +170,17 @@ function insertTime(allEvents, start_or_end, time) {
 };
 
 async function displayAvailabilities(availabilities) {
-	
+
 	let availabilitiesList = document.getElementById('availablilities-list');
 	$(availabilitiesList).empty();
 	let availLength = availabilities.length;
 	for (let i = 0; i < availLength; ++i) {
-		
+
 		let newButton = document.createElement("button");
 		newButton.setAttribute("class", "btn-availability");
 		newButton.onclick = availabilityButton;
 		availabilitiesList.appendChild(newButton);
-		
+
 		let newRow = document.createElement("div");
 		newRow.setAttribute("class", "row");
 		newButton.appendChild(newRow);
